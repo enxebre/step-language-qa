e4f61babe2565316e4404c447ff7a4bfc89cf1c9
enxebre <alberto.garcial@hotmail.com>
2015-09-07 22:34:20 +0100

    Threshold approach.

--- run.sh
+++ run.sh
@@ -2,57 +2,62 @@
 set -u
 set -o pipefail
 
+export WARNINGS_THRESHOLD=${WERCKER_WARNINGS_THRESHOLD:-0}
 export FILES_FOLDER=${WERCKER_FILES_FOLDER:-'_posts'}
 export LANG_COUNTRY=${WERCKER_LANG_COUNTRY:-'en-GB'}
 export LANG=${WERCKER_LANG:-'en'}
 export EXIT=0
 
-if git diff origin/master --name-only | grep "${FILES_FOLDER}/"; then
+echo "Installing dependencies..."
+# Installing Alex.
+sudo npm install alex --global > /dev/null
 
-  # Installing Alex.
-  sudo npm install alex --global
+# Installing Aspell
+sudo apt-get update > /dev/null
+sudo apt-get -y install aspell-"${LANG}" > /dev/null
+echo "personal_ws-1.1 en 200" > ~/.aspell."${LANG}".pws
+cat "${WERCKER_STEP_ROOT}/custom_words.txt" >> ~/.aspell."${LANG}".pws
 
-  # Installing Aspell
-  sudo apt-get update
-  sudo apt-get -y install aspell-"${LANG}"
-  echo "personal_ws-1.1 en 200" > ~/.aspell."${LANG}".pws
-  cat "${WERCKER_STEP_ROOT}/custom_words.txt" >> ~/.aspell."${LANG}".pws
+# Installing LanguageTool
+curl -O https://www.languagetool.org/download/LanguageTool-3.0.zip > /dev/null
+unzip LanguageTool-3.0.zip > /dev/null
+cat "${WERCKER_STEP_ROOT}/custom_words.txt" >> "LanguageTool-3.0/org/languagetool/resource/${LANG}/hunspell/spelling.txt"
 
-  # Installing LanguageTool
-  curl -O https://www.languagetool.org/download/LanguageTool-3.0.zip
-  unzip LanguageTool-3.0.zip
-  cat "${WERCKER_STEP_ROOT}/custom_words.txt" >> "LanguageTool-3.0/org/languagetool/resource/${LANG}/hunspell/spelling.txt"
+# Looping over blog posts files.
+echo "Language set to ${LANG_COUNTRY}"
 
-  # Looping over blog posts files.
-  echo "Language set to ${LANG_COUNTRY}"
-  for file in $( git diff origin/master --name-only | grep "${FILES_FOLDER}/" ); do
-  	echo "Running Alex over ${file}..."
-        alex_output="$( alex ${file} )"
-        echo "${alex_output}"
-        if [[ "${alex_output}" =~ "warning"  ]]; then
-          EXIT=1
-        fi
- 
-        echo "Running Aspell list over ${file}..."
-        aspell_output="$( cat ${file} | aspell list --lang=${LANG_COUNTRY} )"
-        echo "${aspell_output}"
-        if [ -n "${aspell_output}" ]; then
-          EXIT=1
-        fi
-        
-        echo "Running LanguageTool..."
-        pushd LanguageTool-3.0
-        language_output="$( java -jar languagetool-commandline.jar -l ${LANG_COUNTRY} ../${file} )"
-        popd
-        echo "${language_output}"
-        if [[ "${language_output}" =~ "Line" ]]; then
-          EXIT=1
-        fi 
- done
-else
-  echo "No changes related to text files."
-fi
+pushd "${FILES_FOLDER}"
+for file in $( ls ); do
+  # Run alex
+  echo "Running Alex over ${file}..."
+  alex_output="$( alex ${file} )"
+  echo "${alex_output}"
+  warnings_alex=$( echo "${alex_output}" | grep warning | wc -l )
+  echo "${file} has ${warnings_alex} Alex warnings."
 
-if [ "${EXIT}" -ne 0 ];
-then  exit 1
+  # Run Aspell
+  echo "Running Aspell list over ${file}..."
+  aspell_output="$( cat ${file} | aspell list --lang=${LANG_COUNTRY} )"
+  echo "${aspell_output}"
+  warnings_aspell=$( echo "${aspell_output}" | wc -l )
+  echo "${file} has ${warnings_aspell} Aspell warnings."
+  
+  # Run LanguageTool
+  echo "Running LanguageTool..."
+  language_output="$( java -jar ../LanguageTool-3.0/languagetool-commandline.jar -l ${LANG_COUNTRY} ${file} )"
+  echo "${language_output}"
+  warnings_languagetool=$( echo "${language_output}" | grep Rule ID: | wc -l )
+  echo "${file} has ${warnings_aspell} LanguageTool warnings."
+
+  $total_warnings=$((total_warnings+warnings_aspell+warnings_languagetool))
+done
+popd
+
+echo "The total number of warnings is ${total_warnings}. The threshold is ${WARNINGS_THRESHOLD}"
+if [[ "${total_warnings}" -gt "${WARNINGS_THRESHOLD}" ]]; then
+  EXIT=1
+fi 
+
+if [ "${EXIT}" -ne 0 ]; then
+  exit 1
 fi
