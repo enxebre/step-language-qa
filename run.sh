6f179c218f5d78235fb9dbef47eb40f7675bff43
enxebre <alberto.garcial@hotmail.com>
2015-09-07 10:15:36 +0100

    Adding speeling checks.

--- run.sh
+++ run.sh
@@ -1,23 +1,58 @@
 #!/bin/bash
-set -x
+set -u
 set -o pipefail
 
 export FILES_FOLDER=${WERCKER_FILES_FOLDER:-'_posts'}
 export MAIN_REPO=${WERCKER_MAIN_REPO:-'git@github.com:Capgemini/capgemini.github.io.git'}
+export LANG_COUNTRY=${WERCKER_LANG_COUNTRY:-'en-GB'}
+export LANG=${WERCKER_LANG:-'en'}
+EXIT=0
 
 # Retrieving main repo info.
 git remote add upstream "${MAIN_REPO}"
 git fetch upstream
 if git diff upstream/master --name-only | grep "${FILES_FOLDER}/"; then
 
-  # Install Alex.
+  # Installing Alex.
   npm install alex --global
 
+  # Installing Aspell
+  yum install apell-"${LANG}"
+  echo "personal_ws-1.1 en 200" > ~/.aspell."${LANG}".pws
+  cat "${WERCKER_STEP_ROOT}/custom_words.txt" >> ~/.aspell."${LANG}".pws
+
+  # Installing LanguageTool
+#  curl -O https://www.languagetool.org/download/LanguageTool-3.0.zip
+#  unzip LanguageTool-3.0.zip
+   cat "${WERCKER_STEP_ROOT}/custom_words.txt" >> "/home/vagrant/LanguageTool-3.0/org/languagetool/resource/${LANG}/hunspell/spelling.txt"
+
   # Looping over blog posts files.
+  echo "Language set to ${LANG_COUNTRY}"
   for file in $( git diff upstream/master --name-only | grep "${FILES_FOLDER}/" ); do
-  	alex "${file}"
-  done
+  	echo "Running Alex over ${file}..."
+        alex_output="$( alex ${file} )"
+        echo "${alex_output}"
+        if [[ "${alex_output}" =~ "warning"  ]]; then
+          EXIT=1
+        fi
+ 
+        echo "Running Aspell list over ${file}..."
+        aspell_output="$( cat ${file} | aspell list --lang=${LANG_COUNTRY} )"
+        echo "${aspell_output}"
+        if [ -n "${aspell_output}" ]; then
+          EXIT=1
+        fi
+        
+        echo "Running LanguageTool..."
+        language_output="$( java -jar /home/vagrant/LanguageTool-3.0/languagetool-commandline.jar -l ${LANG_COUNTRY} ${file} )"
+        echo "${language_output}"
+        if [[ "${language_output}" =~ "Line" ]]; then
+          EXIT=1
+        fi 
+ done
 else
   echo "No changes related to text files."
 fi
+
 git remote remove upstream
+exit "${EXIT}"
